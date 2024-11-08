63614af351b8890dd2b6799e1951aaed08e80f53
augustorib <augustorifra@gmail.com>
2024-11-08 03:59:10 -0300

    feat: adicionando subtração

--- src/App.js
+++ src/App.js
@@ -31,12 +31,27 @@ const App = () => {
     }
   }
 
+  const handleMinusNumbers = () =>{
+    if(firstNumber == '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    }else {
+        const sum = Number(firstNumber) - Number(currentNumber);
+        setCurrentNumber(String(sum));
+        setOperation('');
+    }
+  }
+
   const handleEquals = () =>{
 
     if(firstNumber != '0' && operation != '' && currentNumber != '0'){
       switch(operation){
         case '+':
           handleSumNumbers();
+          break;
+        case '-':
+          handleMinusNumbers();
           break;
         default:
           break;
@@ -52,14 +67,14 @@ const App = () => {
        <Row>
         <Button label="x"/>
         <Button label="/"/>
-        <Button label="C" onClick={handleOnClear}/>
-        <Button label="CE"/>  
+        <Button label="c" onClick={handleOnClear}/>
+        <Button label="."/>  
        </Row>
        <Row>
         <Button label="7" onClick={() => handleAddNumber('7')} />
         <Button label="8" onClick={() => handleAddNumber('8')}/>
         <Button label="9" onClick={() => handleAddNumber('9')}/>
-        <Button label="-" onClick={() => handleAddNumber('-')}/>
+        <Button label="-" onClick={handleMinusNumbers}/>
        </Row>
        <Row>
         <Button label="4" onClick={() => handleAddNumber('4')} />
